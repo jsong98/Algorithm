d074e31fe6acbee3564a21991b77da31c981c43a
jsong98 <jswsn1526@gmail.com>
2023-10-04 11:17:47 +0900

    20231004 1117

--- src/BaekJoon/BOJ_2096_내려가기.java
+++ src/BaekJoon/BOJ_2096_내려가기.java
@@ -3,6 +3,7 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_2096_내려가기 {
@@ -12,7 +13,7 @@ public class BOJ_2096_내려가기 {
 	static int[][] arr;
 	static int[][] max;
 	static int[][] min;
-	
+	static int[] dc = {-1,0,1};
     
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		int n = Integer.parseInt(br.readLine());
@@ -24,15 +25,44 @@ public class BOJ_2096_내려가기 {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < n; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
+				max[i][j] = arr[i][j];
+				min[i][j] = arr[i][j];
 			}
 		}
 		
-		for(int i = 0; i < n; i++) {
-			max[0][i] = arr[0][i];
-			min[0][i] = arr[0][i];
+		for(int r = 0; r < n-1; r++) {
+			for(int c = 0; c < n; c++) {
+				for(int k = 0; k < 3; k++) {
+					int nr = r + 1;
+					int nc = c + dc[k];
+					if(nc < 0 || nc >= n) continue;
+					int tempMax = max[r][c] + max[nr][nc];
+					int tempMin = min[r][c] + min[nr][nc];
+					if(tempMax > max[nr][nc]) {
+						max[nr][nc] = tempMax;
+						for(int[] test : max) {
+							System.out.println(Arrays.toString(test));
+						}
+						System.out.println();
+					}
+					if(tempMin < min[nr][nc]) {
+						min[nr][nc] = tempMax;
+					}
+				}
+			}
 		}
 		
-		
+//		for(int[] test : arr) {
+//			System.out.println(Arrays.toString(test));
+//		}
+//		System.out.println();
+//		for(int[] test : max) {
+//			System.out.println(Arrays.toString(test));
+//		}
+//		System.out.println();
+//		for(int[] test : min) {
+//			System.out.println(Arrays.toString(test));
+//		}
 	}
 
 }
---
+++ src/BaekJoon/Practice.java
@@ -0,0 +1,29 @@
+package BaekJoon;
+
+import java.util.Arrays;
+
+public class Practice {
+	
+	static int[] arr;
+	static int[] output;
+	
+	public static void main(String[] args) {
+		arr = new int[] {1, 2, 3, 4};
+		output = new int[3];
+		
+		comb(0,0);
+	}
+	
+	public static void comb(int idx, int sidx) {
+		if(sidx == output.length) {
+			System.out.println(Arrays.toString(output));
+			return ;
+		}
+		if(idx == arr.length) return ;
+		
+		output[sidx] = arr[idx];
+		comb(idx+1, sidx+1);
+		comb(idx+1, sidx);
+		
+	}
+}
