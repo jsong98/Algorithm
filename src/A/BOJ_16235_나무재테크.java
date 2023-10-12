8d49040233fd15ff78378f82b6cc0e888cfafccd
jsong98 <jswsn1526@gmail.com>
2023-10-12 16:17:10 +0900

    20231012 1617

---
+++ src/A/BOJ_16235_나무재테크.java
@@ -0,0 +1,15 @@
+package A;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_16235_나무재테크 {
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
--- src/A/BOJ_17471_게리맨더링.java
+++ src/A/BOJ_17471_게리맨더링.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -13,50 +14,48 @@ public class BOJ_17471_게리맨더링 {
 	static StringBuilder sb = new StringBuilder();
 	static int n;
 	static int[] popul;
-	static int[] arr;
+	static int[] combArr;
 	static int[] output;
 	static boolean[] visited;
 	static List<List<Integer>> graph;
-	static List<int[]> combRes;
 	static int min, res;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		n = Integer.parseInt(br.readLine());
 		popul = new int[n];
-		arr = new int[n];
+		combArr = new int[n];
 		graph = new ArrayList<>();
-		combRes = new ArrayList<>();
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < n; i++) {
 			popul[i] = Integer.parseInt(st.nextToken());
-			graph.add(new ArrayList<>());
-			arr[i] = i;
 		}
+		int idx = 1;
 		for(int i = 0; i < n; i++) {
+			combArr[i] = idx++;
+		}
+		for(int i = 0; i <= n; i++) {
+			graph.add(new ArrayList<>());
+		}
+		for(int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
-			int g = Integer.parseInt(st.nextToken());
-			for(int j = 0; j < g; j++) {
-				graph.get(i).add(Integer.parseInt(st.nextToken()));
+			int num = Integer.parseInt(st.nextToken());
+			for(int j = 0; j < num; j++) {
+				int end = Integer.parseInt(st.nextToken());
+				graph.get(i).add(end);
 			}
 		}
 		
-//		for(List<Integer> list : graph) {
-//			for(int t : list) {
-//				System.out.print(t + " ");
-//			}
-//			System.out.println();
-//		}
-		
-		min = Integer.MAX_VALUE;
 		res = -1;
+		min = Integer.MAX_VALUE;
+		
 		for(int i = 1; i <= n/2; i++) {
 			output = new int[i];
 			comb(0,0);
 		}
 		
 		if(res == -1) {
-			System.out.println(-1);
+			System.out.println(res);
 		} else {
 			System.out.println(min);
 		}
@@ -64,65 +63,52 @@ public class BOJ_17471_게리맨더링 {
 	
 	public static void comb(int idx, int sidx) {
 		if(sidx == output.length) {
-			visited = new boolean[n];
-			for(int i : output) {
-				visited[i] = true;
+			visited = new boolean[n+1];
+			for(int i = 0; i < output.length; i++) {
+				visited[output[i]] = true;
 			}
-			
-			int[] rev = new int[arr.length - output.length];
+			int[] rev = new int[n-output.length];
 			int revIdx = 0;
-			for(int i = 0; i < arr.length; i++) {
+			for(int i = 1; i < visited.length; i++) {
 				if(!visited[i]) {
 					rev[revIdx++] = i;
 				}
 			}
-			
-//			for(int i : output) {
-//				System.out.print(i + " ");
-//			}
-//			System.out.print("+");
-//			for(int i : rev) {
-//				System.out.print(i + " ");
-//			}
+//			System.out.println(Arrays.toString(output));
+//			System.out.println(Arrays.toString(rev));
 //			System.out.println();
-
-			if (isConnected(output) && isConnected(rev)) {
-//				for (int i : output) {
-//					System.out.print(i + " ");
-//				}
-//				System.out.print(getSum(output));
-//				System.out.print("+ ");
-//				for (int i : rev) {
-//					System.out.print(i + " ");
-//				}
-//				System.out.print(getSum(rev));
-//				System.out.println();
-				
+			
+			if(isConnected(output) && isConnected(rev)) {
+//				System.out.println(Arrays.toString(output));
+//				System.out.println(Arrays.toString(rev));
 				res = Math.abs(getSum(output) - getSum(rev));
-//				System.out.println(res + " " + min);
+//				System.out.println(res);
 				if(res < min) {
 					min = res;
 				}
 			}
 			
-			return ;
+			return;
 		}
-		if(idx == arr.length) return ;
 		
-		output[sidx] = arr[idx];
+		if(idx == combArr.length) return ;
+		
+		output[sidx] = combArr[idx];
 		comb(idx+1, sidx+1);
 		comb(idx+1, sidx);
 	}
 	
 	public static boolean isConnected(int[] tmp) {
-		if(tmp.length == 1) return true;
+		if(tmp.length==1) return true;
 		
-		boolean[] check = new boolean[n];
+		boolean[] check = new boolean[n+1];
+		List<Integer> tmpList = new ArrayList<>();
 		for(int i = 0; i < tmp.length; i++) {
-			for(int e : graph.get(tmp[i])) {
-				check[e-1] = true;
-			}
+			tmpList.add(tmp[i]);
 		}
+		check[tmp[0]] = true;
+		bfs(check, tmp[0], tmpList);
+		
 		for(int i = 0; i < tmp.length; i++) {
 			if(!check[tmp[i]]) {
 				return false;
@@ -132,11 +118,27 @@ public class BOJ_17471_게리맨더링 {
 		return true;
 	}
 	
+	public static void bfs(boolean[] check, int k, List<Integer> tmpList) {
+		for(int i : graph.get(k)) {
+			if(!tmpList.contains(i)) continue;
+			if(!check[i]) {
+				check[i] = true;
+				bfs(check, i, tmpList);
+			}
+		}
+	}
+
+	public static boolean isConnected(int[] tmp1, int[] tmp2) {
+		
+		
+		return true;
+	}
+
 	public static int getSum(int[] tmp) {
 		int sum = 0;
 		
 		for(int i = 0; i < tmp.length; i++) {
-			sum += popul[tmp[i]];
+			sum += popul[tmp[i]-1];
 		}
 		
 		return sum;
