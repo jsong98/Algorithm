4b5f35abd935563376725562df68355e0396b623
jsong98 <jswsn1526@gmail.com>
2023-10-24 11:22:15 +0900

    20231024 1122

---
+++ src/solvedac/class3/BOJ_117279_최대힙.java
@@ -0,0 +1,40 @@
+package solvedac.class3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class BOJ_117279_최대힙 {
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2-o1;
+			}
+		});
+		
+		for(int i = 0; i < n; i++) {
+			int cmd = Integer.parseInt(br.readLine());
+			if(cmd == 0) {
+				if(pq.isEmpty()) {
+					sb.append(0 + "\n");
+				} else {
+					sb.append(pq.poll() + "\n");
+				}
+			} else {
+				pq.add(cmd);
+			}
+		}
+		
+		System.out.println(sb);
+	}
+
+}
