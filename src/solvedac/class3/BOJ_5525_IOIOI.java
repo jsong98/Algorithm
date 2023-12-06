03bdba1824f1f4de845a040bf5dc52d99c7a8a82
jsong98 <jswsn1526@gmail.com>
2023-12-06 20:29:58 +0900

    boj 5525 sol

--- src/solvedac/class3/BOJ_5525_IOIOI.java
+++ src/solvedac/class3/BOJ_5525_IOIOI.java
@@ -5,35 +5,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BOJ_5525_IOIOI {
-	
+
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
 	static int n, m;
 	static char[] s;
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		n = Integer.parseInt(br.readLine());
 		m = Integer.parseInt(br.readLine());
 		s = br.readLine().toCharArray();
-		
-		char[] target = new char[2*n+1];
-		for(int i = 0; i < target.length; i++) {
-			if(i%2 == 0) {
-				target[i] = 'I';
-			} else {
-				target[i] = 'O';
-			}
-		}
-		
+
 		int res = 0;
-		outer:
-		for(int sIdx = 0; sIdx <= s.length-target.length; sIdx++) {
-			for(int tIdx = 0; tIdx < target.length; tIdx++) {
-//				System.out.println(s.charAt(sIdx+tIdx) + " " + target.charAt(tIdx));
-				if(s[sIdx+tIdx] != target[tIdx]) continue outer;
-				if(tIdx == target.length-1) res++;
+		int cnt = 0;
+
+		for(int i = 1; i < m - 1; i++) {
+			if (s[i - 1] == 'I' && s[i] == 'O' && s[i + 1] == 'I') {
+				cnt++;
+
+				if (cnt == n) {
+					cnt--;
+					res++;
+				}
+				i++;
+			} else {
+				cnt = 0;
 			}
 		}
+
 		System.out.println(res);
 	}
 
