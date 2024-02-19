b30c4ae51945aad3943f577e67005e36eb5fba67
jsong98 <jswsn1526@gmail.com>
2024-02-19 15:36:25 +0900

    20240219 1536

---
+++ src/BaekJoon/BOJ_15644_구슬탈출3.java
@@ -0,0 +1,25 @@
+package BaekJoon;
+
+/*
+input
+ . : 빈칸
+ # : 장애물
+ o : 구멍위치
+ R : 빨간 구슬
+ B : 파란 구슬
++가장 자리는 전부 #
+
+output
+첫째줄: 최소 몇번 만에 빨간 공을 구멍을 통해 빼낼 수 있는지,
+둘째줄: 어떻게 기울여야 하는지 순서대로(LRUD)
++10번 이하로 못빼내면 -1
+
+
+*/
+
+public class BOJ_15644_구슬탈출3 {
+
+    public static void main(String[] args) {
+
+    }
+}
