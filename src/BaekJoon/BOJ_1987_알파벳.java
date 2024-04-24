bed0c022d49bb0b6d19fbe4310438f3971e7c556
송준석 <jswsn1526@gmail.com>
2024-04-24 09:00:40 +0900

    20240424 0900

--- src/BaekJoon/BOJ_1987_알파벳.java
+++ src/BaekJoon/BOJ_1987_알파벳.java
@@ -3,27 +3,57 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_1987_알파벳 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
-    static int r, c;
-    static char[][] map;
-    static int[] dr = {-1,0,1,0};
-    static int[] dc = {0,1,0,-1};
+    static int r, c, max;
+    static String[][] map;
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {-1,0,1,0};
+    static class Pos {
+        int x;
+        int y;
+        String s;
+
+        Pos(int x, int y, String s) {
+            this.x = x;
+            this.y = y;
+            this.s = s;
+        }
+    }
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
-        map = new char[r][c];
-        for (int i = 0; i < r; i++) {
-            map[i] = br.readLine().toCharArray();
-        }   // input
-        for (char[] arr : map) {
-            System.out.println(Arrays.toString(arr));
+        if(r == 1 && c == 1) {
+            System.out.println(1);
+            return ;
         }
-        System.out.println('A'-65);
+        map = new String[r][c];
+        for (int i = 0; i < r; i++) {
+            map[i] = br.readLine().split("");
+        }   // input
+
+        max = Integer.MIN_VALUE;
+        dfs(new Pos(0, 0, ""));
+        System.out.println(max);
+    }
+
+    static void dfs(Pos p) {
+        if(p.s.contains(map[p.y][p.x])) {
+            if(p.s.length() > max) max = p.s.length();
+            return;
+        } else {
+            p.s += map[p.y][p.x];
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nx = p.x+dx[i];
+            int ny = p.y+dy[i];
+            if(nx < 0 || nx >= c || ny < 0 || ny >= r) continue;
+            dfs(new Pos(nx, ny, p.s));
+        }
     }
 }
