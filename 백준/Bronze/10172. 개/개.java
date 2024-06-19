b7e8db6e627121559ce245f883ab2ccdb5786501
jsong98 <79959903+jsong98@users.noreply.github.com>
2024-06-19 23:50:50 +0900

    [Bronze V] Title: 개, Time: 60 ms, Memory: 11396 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder();
+        sb.append("|\\_/|")
+                .append("\n")
+                .append("|q p|   /}")
+                .append("\n")
+                .append("( 0 )\"\"\"\\")
+                .append("\n")
+                .append("|\"^\"`    |")
+                .append("\n")
+                .append("||_/=\\\\__|")
+                .append("\n");
+        System.out.println(sb.toString());
+    }
+}
