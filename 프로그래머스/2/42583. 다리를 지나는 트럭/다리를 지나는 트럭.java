64fe203a1e5befaddefe0b9580ea57b005cf3af2
jsong98 <79959903+jsong98@users.noreply.github.com>
2024-10-14 16:20:55 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 21.29 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+    
+    class Truck {
+        int w;
+        int t;
+        
+        public Truck(int w, int t) {
+            this.w = w;
+            this.t = t;
+        }
+    }
+    
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Queue<Truck> onBridge = new LinkedList<Truck>();
+        Queue<Integer> trucks = new LinkedList<Integer>();
+        for(int i = 0; i < truck_weights.length; i++) {
+            trucks.offer(truck_weights[i]);
+        }
+        int totalWeight = 0;
+        int time = 0;
+        
+        while(true) {
+            time++;
+            
+            if(!onBridge.isEmpty()) {   // 다리에서 트럭이 내려오는 로직
+                Truck truck = onBridge.peek();
+                if(time - truck.t >= bridge_length) {
+                    // System.out.println("off " + time + " " + truck.t);
+                    onBridge.poll();
+                    totalWeight -= truck.w;
+                    if(onBridge.isEmpty() && trucks.isEmpty()) return time;
+                }
+            }
+            
+            if(!trucks.isEmpty()) {     // 다리에 트럭이 올라가는 로직
+                int truckWeight = trucks.peek();
+                if(totalWeight + truckWeight <= weight) {
+                    // System.out.println("on " + time);
+                    onBridge.offer(new Truck(truckWeight, time));
+                    totalWeight += truckWeight;
+                    trucks.poll();
+                }
+            }
+            
+        }
+        
+        
+        // return answer;
+    }
+}
