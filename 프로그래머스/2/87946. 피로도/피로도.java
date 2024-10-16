e15d28a98aef76c98e79262e03b041e8fecb2a39
jsong98 <79959903+jsong98@users.noreply.github.com>
2024-10-16 16:38:09 +0900

    [level 2] Title: 피로도, Time: 2.82 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,38 +1,18 @@
 import java.util.*;
 class Solution {
-    int answer;
-    int t;
     public int solution(int k, int[][] dungeons) {
-        answer = -1;
-        t = -1;
-        boolean[] visited = new boolean[dungeons.length];
-        int len = dungeons.length;
-        perm(dungeons, new boolean[len], new int[len][2], 0, len, k);
-        return answer;
+        return dfs(k, dungeons);
     }
-    
-    public void perm(int[][] dungeons, boolean[] visited, int[][] arr, int depth, int r, int k) {
-        if(depth == r) {
-            int count = 0;
-            t = k;
-            for(int[] dungeon : arr) {
-                if(t >= dungeon[0]) {
-                    count++;
-                    t -= dungeon[1];
-                    continue;
-                }
-                break;
-            }
-            if(count > answer) answer = count;
-            return ;
-        }
-        for(int i = 0; i < dungeons.length; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                arr[depth] = dungeons[i];
-                perm(dungeons, visited, arr, depth+1, r, k);
-                visited[i] = false;                
+    public int dfs(int k, int[][] dungeons) {
+        int cnt = 0;
+        for(int[] d : dungeons) {
+            int p = d[0], q = d[1];
+            if(k >= p) {
+                d[0] = 10000;
+                cnt = Math.max(1 + dfs(k-q, dungeons), cnt);
+                d[0] = p;
             }
         }
+        return cnt;
     }
 }
